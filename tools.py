8c9fd66561e8ec398871cc772ab5e69cb5755a3f
jame windows10 <thanakornn.ch@gmail.com>
2022-10-10 01:09:15 +0700

    update logic

--- tools.py
+++ tools.py
@@ -1,4 +1,7 @@
 
+from email.headerregistry import AddressHeader
+from ssl import ALERT_DESCRIPTION_ACCESS_DENIED
+from urllib.request import AbstractDigestAuthHandler
 import pyperclip
 import keyboard
 import pandas as pd
@@ -7,11 +10,14 @@ from sys import platform
 if platform =='win32':
     import win32api
     win32api.LoadKeyboardLayout('00000409',1) # to switch to english
-    cm_copy ="ctrl+v"
-    cm_del ="\b"
-elif platform =='darwin':
-    cm_copy ="command+v"
-    cm_del ="delete"
+import time
+
+def back_space(action):
+    global history, cm_del
+    if action :
+        keyboard.send(cm_del)
+    history='0'+history[:len(history)-1]
+    print('del :',history)
 
 def chang_command_mac(command_list):
     new_command_list =[]
@@ -37,9 +43,18 @@ def chang_command_mac(command_list):
 def load_data_excel(platform):
     data = pd.read_excel('./memo/memo.xlsx')
     data = data.dropna()
-    #? need to update : sort
+
+    lenght_command=[]
+    for text in data['command']:
+        lenght_command.append(len(text))
+
+    data['lenght_command'] = lenght_command
+    data = data.sort_values(by='lenght_command', ascending=False)
+
     message_json = data['message'].values.tolist()
     command_list = data['command'].values.tolist()
+    
+    # air_quality["london_mg_per_cubic"] = air_quality["station_london"] * 1.882
 
     if platform == 'darwin':
         command_list = chang_command_mac(command_list)
@@ -47,43 +62,57 @@ def load_data_excel(platform):
     return command_list, message_json
 
 def write(replacement,command):
-    global history, cm_copy, cm_del
+    global history, cm_paste, cm_del
     history ="0123456"
-    #? need to update : find
+
+    save_old_copy = pyperclip.paste()
     for n in range(len(command)):
         keyboard.send(cm_del)
-    
+
     pyperclip.copy(replacement)
-    keyboard.send(cm_copy)
+    keyboard.send(cm_paste)
+    time.sleep(0.2)
+    pyperclip.copy(save_old_copy)
+
 
 def check_map_command(history):
     global command_list,message_json
-
+    is_match = False
     for index,command in enumerate(command_list) :
+        print(command)
         if command in history :
-            print('true',command,history)
             write(message_json[index],command_list[index])
+            is_match = True
+            break
+    
+    if not is_match:
+        back_space(True)
 
-def released(release, history):
-    global cm_del
+def released(release):
+    global cm_del,history
 
     if(len(release))==1:
         history=history[1:]+release
         print(history)
     elif release=='right shift':
-        if history=='0123456':
-            keyboard.send(cm_del)
-        else :
-            check_map_command(history)
+        check_map_command(history)
+    elif release=='backspace':
+        back_space(False)
     else :
         print(release)
-        # keyboard.send(cm_del)
 
 def main(history):
-    keyboard.on_release(lambda e: released( e.name ,history)) # old logic keyboard.add_abbreviation('@@', 'my.long.email@example.com')
+    keyboard.on_release(lambda e: released( e.name )) # old logic keyboard.add_abbreviation('@@', 'my.long.email@example.com')
     keyboard.wait()
 
-command_list ,message_json= load_data_excel( platform)  #load data from excel file.
-history ="0123456"
 
-main(history) 
+if platform =='win32':
+    cm_paste ="ctrl+v"
+    cm_del ="\b"
+elif platform =='darwin':
+    cm_paste ="command+v"
+    cm_del ="delete"
+
+command_list ,message_json= load_data_excel(platform)
+history ="0123456"
+main(history)
