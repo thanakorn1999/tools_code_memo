06bd8b54095e2343d43aa76e300ead905b9c178e
jame windows10 <thanakornn.ch@gmail.com>
2023-05-26 23:22:09 +0700

    add bat file

--- start.bat
+++ start.bat
@@ -1,6 +1,5 @@
-
 @echo off
 cd C:\Users\mrjam\OneDrive\เดสก์ท็อป\Jame\code\tools_code_memo
-echo env_code_memo\Scripts\activate.bat
+echo C:\Users\mrjam\OneDrive\เดสก์ท็อป\Jame\code\tools_code_memo\env_code_memo\Scripts\activate.bat
 echo python tools.py
 cmd /k 
